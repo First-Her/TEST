427e4e6acb1f4c95331e2ed8ebcdfe73dc4c610a
First-Her <dima.rostov.911@mail.ru>
2024-09-14 22:11:52 +0300

    lESS CALL

---
+++ less 22/classwork/main.js
@@ -0,0 +1,10 @@
+let name = 'Джон';
+let admin = name;
+console.log(admin);
+
+let earth = 'земля';
+let heavenlybody = 'земля';
+console.log(earth);
+console.log(heavenlybody);
+
+let num = 5.5;
---
+++ less21/js/main.js
@@ -0,0 +1,40 @@
+//console.warn("java");
+//document.write('java');
+//console.log('java');
+//console.info('java');
+//console.error('java');
+//var num; 
+//num = 5;
+//var num = 5;
+//num = 7;
+//console.log(num);
+//console.log('переменная: ' + num);
+//const num = 5;
+//var num_1 = 5;
+//var num_2 = 15;
+//console.log('вычитание: ' + (num_1 - num_2));
+//var num_3 = 5;
+//num_3 = num_3 +7;
+//console.log('результат: ' + num_3);
+//var str_1 = Number('12');
+//var str_2 = Number('2');
+//console.log('Результат:' + (str_1 + str_2));
+//console.log('math:' + Math.PI);
+//console.log('math:' + Math.min(4, 6, 8,));
+
+//var Number = 15;
+//var ishashous = true;
+
+//if(Number == 15 && ishashous)  {
+   // console.log('ok');
+//} else {
+    //console.log('error!');
+//}
+var stroka = 'word';
+switch(stroka) {
+    case"4": console.log('переменная со значением 4');
+    break;
+    case"45":
+    console.log('переменная со значением 45');
+    break;
+}
