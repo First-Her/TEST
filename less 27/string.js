9405ec8f39e1f217d18da4517d291c48f3e115b1
First-Her <dima.rostov.911@mail.ru>
2024-10-02 14:59:26 +0300

    Стринги

--- less 27/string.js
+++ less 27/string.js
@@ -52,3 +52,45 @@ const resulThree =
   "-" +
   funny.slice(6, 10).toUpperCase();
 console.log(resulThree);
+
+const uzerName = "вася";
+const result =
+  uzerName.slice(0, 1).toUpperCase() +
+  uzerName.slice(1, 3) +
+  uzerName.slice(3, 4).toUpperCase();
+console.log(result);
+
+const greetings = "Привет";
+console.log(greetings);
+
+const wordOne = greetings.slice(0, 7);
+console.log(wordOne);
+
+const wordTwo =
+  greetings[0].toLowerCase() + "a" + "к" + greetings[4] + greetings[5] + ",";
+console.log(wordTwo);
+
+const wordThree =
+  greetings[0].toLowerCase() +
+  greetings[2] +
+  "с" +
+  greetings[5] +
+  "о" +
+  "л" +
+  greetings[4] +
+  greetings[5] +
+  ",";
+console.log(wordThree);
+
+const wordFour =
+  greetings[0].toLowerCase() +
+  greetings[4] +
+  "ч" +
+  greetings[4] +
+  "н" +
+  greetings[4] +
+  "г";
+console.log(wordFour);
+
+const rezultGreetings =  `"${wordOne} ${wordTwo} ${wordThree} ${wordFour}"`;
+console.log(rezultGreetings);
