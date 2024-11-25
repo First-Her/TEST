0541ed5c2113efb2c9199d7d31295753f128b5d8
First-Her <dima.rostov.911@mail.ru>
2024-11-25 23:05:41 +0300

    задачи

---
+++ less 28/level.1.7.js
@@ -0,0 +1,48 @@
+// №1
+// Дана строка:
+// 'abcde'
+// Получите массив букв этой строки.
+// let listStrings = ['a','b','c','d','e'];
+// console.log(listStrings)
+
+// let letters = 'abcde';
+// console.log(letters.split(''))
+// // let listStrings = letters.split(''); можно так
+
+// let letters = 'abcde';
+// // let listStrings = letters.split('');  можно так
+// // console.log(listStrings)
+// let listStrings = [];
+// for (let i = 0; i < letters.length; i++)  {
+//     listStrings.push(letters[i])
+//     // console.log(letters[i]);
+// }
+// console.log(listStrings)
+
+// №2
+// Дано некоторое число:
+// 12345
+// Получите массив цифр этого числа.
+// let number = 12345;
+// let listNumber = [];
+// let stringNumber = number.toString();
+// for (let i = 0; i < stringNumber.length; i++) {
+//     listNumber.push(Number(stringNumber[i]))
+//     // listNumber.push(+number.toString()[i])
+// }
+// console.log(listNumber)
+
+// №3
+// Дано некоторое число:
+// 12345
+// Переверните его:
+// 54321
+// let number = 12345;
+// let listNumber = [];
+// let stringNumber = number.toString();
+// for (let i = 0; i < stringNumber.length; i++) {
+//   listNumber.unshift(Number(stringNumber[i]));
+// }
+// console.log(listNumber);
+// let revrNunber = listNumber.join("");
+// console.log(Number(revrNunber));
--- less 28/level1.6.js
+++ less 28/level1.6.js
@@ -1,9 +1,36 @@
 // №1
 // Дан массив с числами. Найдите сумму квадратов элементов этого массива.
 // let sunNum = 0
-// const arrNum = [1, 2, 3] 
+// const arrNum = [1, 2, 3]
 // for (let i = 0; i < arrNum.length; i++) {
-//     sunNum = sunNum + arrNum[i] **2; 
+//     sunNum = sunNum + arrNum[i] **2;
 // }
 // console.log(sunNum)
 // // console.log(3 ** 2);
+
+// №3
+// Дан массив с числами. Найдите сумму положительных элементов этого массива.
+// let result = 0;
+// const arrNumber = [2, 6, -9, 10];
+// for (let i = 0; i < arrNumber.length; i++) {
+//   if (arrNumber[i] > 0) {
+//     result = result + arrNumber[i];
+//   }
+//   // console.log(arrNumber)
+//   // console.log(result)
+// }
+// console.log(result);
+
+// №4
+// Дан массив с числами. Найдите сумму тех элементов этого массива, которые больше нуля и меньше десяти.
+// let result = 0;
+// const arrNumbers = [2, 6, 3, 9, 12, 13];
+// for (let i = 0; i < arrNumbers.length; i++) {
+//     // console.log(arrNumbers)
+//     if (arrNumbers[i] > 0 && arrNumbers[i] < 10) {
+//         result = result + arrNumbers[i]
+//     }
+// }
+// console.log(result)
+// console.log(arrNumbers)
+
