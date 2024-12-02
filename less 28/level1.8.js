787c9e10534f4d1e017eb1457bf7d16d438d3c6a
First-Her <dima.rostov.911@mail.ru>
2024-12-02 18:34:19 +0300

    задачи

--- less 28/level1.8.js
+++ less 28/level1.8.js
@@ -9,10 +9,8 @@
 // №2
 // Заполните массив четными числами из промежутка от 1 до 100.
 // let listNumber = [];
-// let numbers = 0
 // for (let i = 1; i <= 100; i++) {
-//   if (i % 2 === 0) numbers = listNumber.push(i);
-
+//   if (i % 2 === 0) listNumber.push(i);
 // }
 // console.log(listNumber)
 
@@ -20,13 +18,22 @@
 // Дан массив с дробями:
 // [1.456, 2.125, 3.32, 4.1, 5.34]
 // Округлите эти дроби до одного знака в дробной части.
+// !!! образец 1
 // let listNumber = [1.456, 2.125, 3.32, 4.1, 5.34];
-// let listresult = [];
+// let listResult = [];
 // function rounding() {
 //   for (let i = 0; i < listNumber.length; i++) {
 //     let result = Number(listNumber[i].toFixed(1));
-//     listresult.push(result);
+//     listResult.push(result);
 //   }
 // }
 // rounding();
-// // console.log(listresult);
+// console.log(listResult);
+
+// !!! образец 2
+// let listNumber = [1.456, 2.125, 3.32, 4.1, 5.34];
+// function rounding() {
+//   const newArr = listNumber.map((item) => Number(item.toFixed(1)));
+//   console.log(newArr);
+// }
+// rounding();
--- less 28/level1.9.js
+++ less 28/level1.9.js
@@ -1,9 +1,42 @@
 // №1
 // Дан массив со строками. Оставьте в этом массиве только те строки, которые начинаются на http://.
-let listNumber = [
-  "https://www.example.com",
-  "http://google.com",
-  "ftp://test.com",
-  "http://github.com",
-  "file:///local/file.txt",
-];
+// let listNumber = [
+//   "https://www.example.com",
+//   "http://google.com",
+//   "ftp://test.com",
+//   "http://github.com",
+//   "file:///local/file.txt",
+// ];
+// let result = []
+// for (let i = 0; i < listNumber.length; i++) {
+// if (listNumber[i].startsWith("http://")) {
+//   result.push(listNumber[i])
+// }
+// }
+// console.log(result)
+
+// №2
+// Дан массив со строками. Оставьте в этом массиве только те строки, которые заканчиваются на .html.
+// let listNumber = [
+//   "https://www.example.com",
+//   "http://google.com",
+//   "ftp://test.com",
+//   "http://github.com",
+//   "file:///local/file.html",
+// ];
+// let result = []
+// for (let i = 0; i < listNumber.length; i++) {
+// if (listNumber[i].endsWith("html")) {
+//   result.push(listNumber[i])
+// }
+// }
+// console.log(result)
+
+// №3
+// Дан массив с числами. Увеличьте каждое число из массива на 10 процентов.
+// let listNumber = [1, 2, 3, 4, 5];
+// let result = []
+// for (let i = 0; i < listNumber.length; i++) {
+//     result.push((listNumber[i]) * 1.1);
+// }
+// console.log(result)
