96b2f7d1af09f5ca1398e10277c8c6829d57ae27
First-Her <dima.rostov.911@mail.ru>
2024-12-02 19:50:14 +0300

    задачи

---
+++ less 28/level1.10.js
@@ -0,0 +1,18 @@
+// №1
+// Заполните массив случайными числами из промежутка от 1 до 100.
+// let listNumber = [];
+// for (let i = 1; i < 5; i++) listNumber.push(Math.ceil(Math.random() * 100));
+// console.log(listNumber);
+
+// №2
+// Дано некоторое число:
+// 12345
+// Выведите в консоль все его символы с конца.
+// let number = 12345;
+// let listNumber = [];
+// let str = "";
+// let stringNumber = number.toString();
+// for (let i = stringNumber.length - 1; i >= 0; i--) {
+//   str += stringNumber[i];
+// }
+// console.log(str);
