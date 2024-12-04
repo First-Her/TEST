8fb34c01af4893556f98e2e4f7341f68d0f8da85
First-Her <dima.rostov.911@mail.ru>
2024-12-04 21:54:31 +0300

    задачи

---
+++ less 28/level2.1.js
@@ -0,0 +1,16 @@
+// №1
+// Дана некоторая строка. Найдите позицию первого нуля в строке.
+// 'ab0cd0e'
+// const str = "ab0cd0e";
+// console.log(str.indexOf('0'))
+
+// №4
+// Дан некоторый массив, например, вот такой:
+// [1, 2, 3, 4, 5, 6]
+// Найдите сумму первой половины элементов этого массива.
+// let listNumber = [1, 2, 3, 4, 5, 6];
+// let result = 0;
+// for (let i = 0; i < listNumber.slice(3).length; i++) {
+//     result += listNumber[i]
+// }
+// console.log(result)
---
+++ less 28/level2.2.js
@@ -0,0 +1,20 @@
+// №1
+// Дан массив с числами. Подсчитайте количество отрицательных чисел в этом массиве.
+// let listNumber = [1, -2, -3, -4, 5, -6];
+// let result = 0;
+// for (let i = 0; i < listNumber.length; i++) {
+//   if (listNumber[i] < 0) result++;
+// }
+// console.log(result);
+
+// №2
+// Дан массив с числами. Оставьте в нем только положительные числа.
+// let listNumber = [1, -2, -3, -4, 5, -6];
+// let positive = [];
+// for (let i = 0; i < listNumber.length; i++) {
+//   if (listNumber[i] > 0) {
+//     positive.push(listNumber[i]);
+//   }
+// }
+// console.log(positive);
+
